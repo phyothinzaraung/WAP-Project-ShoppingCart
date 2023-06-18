2a6c0bdc1a5b8c0499a75a69d1ee1af103b40ac9
Phyo Thinzar Aung <phyothinzar.ag92@gmail.com>
2023-06-18 10:35:16 -0500

    images commit

--- shopping-server/app.js
+++ shopping-server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+const path = require('path');
 
 const productRouter = require("./routes/productRouter");
 const userRouter = require("./routes/userRouter");
@@ -13,6 +14,9 @@ app.use(cors());
 app.use("/api/products", productRouter);
 app.use("/api/login", userRouter);
 app.use("/api/shopping-carts", shoppingCartRouter);
+app.use("/images/:imagename", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "images", req.params.imagename));
+});
 
 app.use((req, res, next) => {
     res.status(404).send("Api is not supported");
